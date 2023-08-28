27dd0e87e0c740511172246bf52fc6e6a4feaf6c
VivianaMuñozCastrillon <vivi84castri@gmail.com>
2023-08-28 08:43:52 -0500

    Agregar archivos de proyecto.

---
+++ frmTienda/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace frmTienda
+{
+    public partial class Form1 : Form
+    {
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTotal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            clsTipo4 camisa = new clsTipo4(txtNombre.Text,Convert.ToInt32( txtPrecio.Text), Convert.ToInt32(cboTipo.Text), Convert.ToInt32(txtIva.Text));
+            clsTipo7 pantalon = new clsTipo7(txtNombre.Text, Convert.ToInt32(txtPrecio.Text), Convert.ToInt32(cboTipo.Text), Convert.ToInt32(txtIva.Text));
+            clsTipo16 camiseta = new clsTipo16(txtNombre.Text, Convert.ToInt32(txtPrecio.Text), Convert.ToInt32(cboTipo.Text), Convert.ToInt32(txtIva.Text));
+
+            
+
+            if (cboTipo.SelectedIndex == 1)
+            {
+                double precioDescontado = camisa.precio1(Convert.ToDouble(txtPrecio.Text), Convert.ToInt32(txtIva.Text));
+                MessageBox.Show("El precio total a pagar por la camisa es: "+"" + precioDescontado);
+               
+            }
+
+            else if (cboTipo.SelectedIndex == 2)
+            {
+                double precioDescontado = pantalon.precio1(Convert.ToDouble(txtPrecio.Text), Convert.ToInt32(txtIva.Text));
+                MessageBox.Show( "El precio total a pagar por el pantalon es: " + "" + precioDescontado);
+               
+            }
+
+            else if (cboTipo.SelectedIndex == 3)
+            {
+                double precioDescontado = camiseta.precio1(Convert.ToDouble(txtPrecio.Text), Convert.ToInt32(txtIva.Text));
+                MessageBox.Show( "El precio total a pagar por la camiseta es: " + "" + precioDescontado);
+                
+            }
+          
+
+
+
+
+
+        }
+
+        private void txtPrecio_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cboTipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            int indice = cboTipo.SelectedIndex;
+            int precio = 0;
+            string nombre="";
+            switch (indice)
+            {
+                case 1:
+                    precio = 45000;
+                    nombre = "Camisa";
+                    break;
+                case 2:
+                    precio = 67000;
+                    nombre = "Pantalon";
+                    break;
+                case 3:
+                    precio = 89000;
+                    nombre = "Camiseta";
+                    break;
+            }
+            txtPrecio.Text=precio.ToString();
+            txtNombre.Text = nombre;
+
+           
+        }
+
+        private void txtPrecio_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
---
+++ frmTienda/clsArticulo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frmTienda
+{
+     abstract class clsArticulo
+     {
+        private  string nombre;
+        private double precio;
+        private double iva;
+        private int tipo;
+        
+
+        public clsArticulo(string nombre, double precio, double iva, int tipo)
+        {
+            this.Nombre = nombre;//Está asignando el valor del parámetro nombre al atributo Nombre del objeto actual (this).
+            this.Precio = precio;
+            this.Iva = iva;
+            this.Tipo = tipo;
+            
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public double Precio { get => precio; set => precio = value; }
+        public double Iva { get => iva; set => iva = value; }
+        public int Tipo { get => tipo; set => tipo = value; }
+
+        public abstract double precio1(double descuento, double iva);
+
+        //public abstract double precio1(double precio, double descuento, double iva)
+        //{
+        //    double montoIVA = precio * (iva / 100);
+        //    double precioConIVA = precio + montoIVA;
+        //    double montoDescuento = precioConIVA * (descuento / 100);
+        //    double precioFinal = precioConIVA - montoDescuento;
+        //    return precioFinal;
+
+
+
+        //}
+
+
+    }
+}
